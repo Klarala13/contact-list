71ed34bf1cdd5869cb17b5f11aae6364bcd5fb1e
Klara <sanchez.clara13@gmail.com>
2020-12-08 15:26:53 +0100

    Updates

---
+++ precache-manifest.518cf7532e9f2d77c062a72d7458d574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddef6ba178220597addfcb6f1724be1",
+    "url": "/contact-list/index.html"
+  },
+  {
+    "revision": "b297c9e66d4fc1070f97",
+    "url": "/contact-list/static/css/main.474c4da0.chunk.css"
+  },
+  {
+    "revision": "7361940e23eacd8f0d38",
+    "url": "/contact-list/static/js/2.89ee6aed.chunk.js"
+  },
+  {
+    "revision": "b297c9e66d4fc1070f97",
+    "url": "/contact-list/static/js/main.6730fb0e.chunk.js"
+  },
+  {
+    "revision": "a13ef1640f1dff929b68",
+    "url": "/contact-list/static/js/runtime~main.ec816a4f.js"
+  }
+]);
